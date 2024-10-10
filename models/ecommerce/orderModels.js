2058058c13d054e2ab2e3d7775cefc667edc7bab
milanbhandari047 <milanbhandari047@gmail.com>
2024-10-10 20:31:08 +0545

    ecommerce data modeling

---
+++ models/ecommerce/orderModels.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Shipped", "Delivered", "Cancelled"],
+      default: "Pending",
+    },
+    shippingAddress: {
+      type: String,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["Cash On Delivery", "Khalti", "Esewa"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
